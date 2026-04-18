c267186c8fab012bbcad868ad2b01f6411f4f133
Vaishnavi1612-star <vaishnavikothawade16@gmail.com>
2026-04-18 14:22:46 +0530

    Update and rename App.js + App.css to App.js

--- src/App.js
+++ src/App.js
@@ -51,23 +51,5 @@ function App() {
     </div>
   );
 }
+
 export default App;
-.app {
-  min-height: 100vh;
-  display: flex;
-  flex-direction: column;
-}
-
-.app-main {
-  flex: 1;
-  padding: 32px 40px;
-  max-width: 1400px;
-  margin: 0 auto;
-  width: 100%;
-}
-
-@media (max-width: 768px) {
-  .app-main {
-    padding: 20px 16px;
-  }
-}
