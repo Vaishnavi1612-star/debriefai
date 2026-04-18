f6373d464bf90e88b296020388049e6bfda66dc6
Vaishnavi1612-star <vaishnavikothawade16@gmail.com>
2026-04-18 16:36:12 +0530

    Add files via upload

---
+++ client/src/App.js
@@ -0,0 +1,55 @@
+import React, { useState } from 'react';
+import Navbar from './components/Navbar';
+import Dashboard from './components/Dashboard';
+import AnalysisPage from './components/AnalysisPage';
+import ReportPage from './components/ReportPage';
+import SettingsPage from './components/SettingsPage';
+import './App.css';
+
+function App() {
+  const [activePage, setActivePage] = useState('dashboard');
+  const [analysisData, setAnalysisData] = useState(null);
+  const [isAnalyzing, setIsAnalyzing] = useState(false);
+
+  const handleAnalysisComplete = (data) => {
+    setAnalysisData(data);
+    setActivePage('analysis');
+  };
+
+  const handleStartAnalysis = () => {
+    setIsAnalyzing(true);
+  };
+
+  const renderPage = () => {
+    switch (activePage) {
+      case 'dashboard':
+        return (
+          <Dashboard
+            onAnalysisComplete={handleAnalysisComplete}
+            isAnalyzing={isAnalyzing}
+            setIsAnalyzing={setIsAnalyzing}
+            analysisData={analysisData}
+          />
+        );
+      case 'analysis':
+        return <AnalysisPage data={analysisData} />;
+      case 'report':
+        return <ReportPage analysisData={analysisData} />;
+      case 'settings':
+        return <SettingsPage />;
+      default:
+        return <Dashboard onAnalysisComplete={handleAnalysisComplete} />;
+    }
+  };
+
+  return (
+    <div className="app">
+      <Navbar activePage={activePage} setActivePage={setActivePage} />
+      <main className="app-main">
+        {renderPage()}
+      </main>
+    </div>
+  );
+}
+
+export default App;
